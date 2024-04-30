3dcca2540185369b159e577c0785db6488e926ec
huguesLouba10 <117634090+huguesLouba10@users.noreply.github.com>
2024-04-30 13:03:37 +0300

    first commit
    first commit

---
+++ Assets/cameramovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    public float minDistance = 1.0f;
+    public float maxDistance = 4.0f;
+    public float smooth = 10.0f;
+    Vector3 dollyDir;
+    public Vector3 dollyDirAdjusted;
+    public float distance;
+
+    void Awake()
+    {
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+
+    void Update()
+{
+    if (transform.parent == null)
+    {
+        Debug.LogError("Parent transform is null");
+        return; // Exit Update if there's no parent to avoid further errors
+    }
+
+    Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+    RaycastHit hit;
+
+    if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit))
+    {
+        distance = Mathf.Clamp((hit.distance * 0.9f), minDistance, maxDistance);
+    }
+    else
+    {
+        distance = maxDistance;
+    }
+
+    transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+}
+}
